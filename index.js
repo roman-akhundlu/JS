f095de97b9e3c130d96256fa9ab7a9bb48c64af3
roman-akhundlu <romanakhundlu@gmail.com>
2024-03-02 23:17:23 +0400

    javascript

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const telebeAdi = "preston"
+const telebeSoyadi = "BIG"
+const telebeTevelludu = 1995
+let isStudent = false
+
+
+const telebe = [
+    {
+        ad: "preston",
+        soyad: "BIG",
+        tevellud: 1995,
+        isStudent: false,
+        telebeninYashiniHesabla: function() {
+            return new Date().getFullYear()  - this.tevellud
+        }
+    },
+    
+    {
+        ad: "Splash",
+        soyad: "Alvin",
+        tevellud: 2000,
+        isStudent: true,
+        telebeninYashiniHesabla: function() {
+            return new Date().getFullYear()  - this.tevellud
+        }
+    },
+
+    {
+        ad: "Khandur",
+        soyad: "tdur",
+        tevellud: 1950,
+        isStudent: false,
+        telebeninYashiniHesabla: function() {
+            return new Date().getFullYear()  - this.tevellud
+        }
+    }
+]
+
+for(let i=0; i<telebeler.length ; i++ ) {
+    console.log(telebe[i].ad)
+}
+
